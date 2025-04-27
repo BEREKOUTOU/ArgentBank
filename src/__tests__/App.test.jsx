3c2b1a0c805cad3d7a5d7ab76469cef85acc0a2e
BEREKOUTOU <166011351+BEREKOUTOU@users.noreply.github.com>
2025-04-27 18:22:34 +0200

    test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { test, expect } from '@jest/globals';
 import App from '../App';
 
 test('renders learn react link', () => {
